58f8bfb03efdadfecdc0af0bfdee2510763c3359
Gamerein <reinrijn@gmail.com>
2018-05-30 21:11:21 +0200

    Initial commit

---
+++ public_html/assets/particleSettings.js
@@ -0,0 +1,16 @@
+particlesJson = {
+	"particles":{
+		"number":{
+			"value":120
+		},
+		"opacity":{
+			"value":0.5
+		},
+		"size":{
+			"value":2
+    },
+    "move":{
+      "speed": 1
+    }
+	}
+}
